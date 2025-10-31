bc27dd1f4d41e5ec81748937e532250c296c2c02
ahmetakyurt <129226079+ahmetakyurt@users.noreply.github.com>
2025-10-31 09:24:30 +0300

    numberGuuesser.py added

---
+++ numberGuesser.py
@@ -0,0 +1,85 @@
+import random
+
+'''
+random_number = random.randint(1, 100)
+attempts = 0
+print("Welcome to the Number Guesser Game!")
+while True:
+    user_guess = input("Guess a number between 1 and 100: ")
+    attempts += 1
+    try:
+        user_guess = int(user_guess)
+    except ValueError:
+        print("Please enter a valid integer.")
+        continue
+
+    if user_guess < random_number:
+        print("Too low! Try again.")
+    elif user_guess > random_number:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You've guessed the number {random_number} in {attempts} attempts.")
+        break
+    '''
+
+print("Welcome to number guessing game!")
+print("The number is going to be between 0 and 100")
+
+randomNum = 69
+guessCount = 0
+
+def optimalCount(number):
+    guessCount = 0
+    guessedNumber1 = 50
+    guessedNumber2 = 50
+    guessedNumber3 = 50
+    while True:
+        guessCount +=1
+        if guessedNumber1 == randomNum:
+            print("Great! You won.")
+            break
+        elif guessedNumber1 < number: #yukarı
+            if guessedNumber1 < guessedNumber3:
+                guessedNumber2 = guessedNumber1
+                guessedNumber1 = int((guessedNumber3 + guessedNumber2)/2)
+                
+            elif guessedNumber1 < guessedNumber2:
+                guessedNumber3 = guessedNumber1
+                guessedNumber1 = int((guessedNumber2 + guessedNumber1)/2)
+            else:
+                guessedNumber1 = guessedNumber1/2 + 50
+                
+                
+        else: #aşağı
+            if guessedNumber1 > guessedNumber3:
+                guessedNumber2 = guessedNumber1
+                guessedNumber1 = int((guessedNumber3 + guessedNumber2)/2)
+                
+            elif guessedNumber1 > guessedNumber2:
+                guessedNumber3 = guessedNumber1
+                guessedNumber1 = int((guessedNumber + guessedNumber1)/2)
+            else:
+                guessedNumber1 = guessedNumber1/2 
+                
+    return guessCount
+
+
+while True:
+    print("Enter your guess: ")
+    guessedNumber = int(input())
+    guessCount += 1
+
+    if not guessedNumber < 100 or not guessedNumber>=0:
+        print("Please enter a number between 0 and 100!")
+    elif guessedNumber == randomNum:
+        print("Great! You won.")
+        break
+    elif guessedNumber > randomNum:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again.")
+
+print("Total attampts: ", guessCount)
+print("The optimum attempts it would take: " + str(optimalCount(randomNum)))
+
+
